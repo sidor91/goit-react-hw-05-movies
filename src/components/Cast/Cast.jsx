7494a74014b7bd08d127c97690932911f1cd463a
Serhii Sydorenko <sidorchik91@gmail.com>
2023-03-30 22:50:04 +0300

    added check for cast abcense

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -11,7 +11,8 @@ const Cast = () => {
   useEffect(() => {
     movieCastApi(movieId)
       .then(response => {
-        setCast(response);
+          setCast(response);
+          console.log('response', response)
       })
       .catch(error => setError(error.message));
   }, [movieId]);
@@ -24,26 +25,34 @@ const Cast = () => {
     <h2>{error}</h2>
   ) : (
     cast && (
-      <div>
-        <List>
-          {cast.cast.map(({ cast_id, profile_path, name, character }) => (
-            <ListItem key={cast_id}>
-              <Photo
-                src={
-                  profile_path
-                    ? `${IMAGE_BASE_URL}${profile_path}`
-                    : `${PHOTO_PLACEHOLDER}`
-                }
-                alt=""
-              />
-              <Text>{name}</Text>
-              <Text>
-                <b>Character</b>: {character}
-              </Text>
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      <>
+        {cast.cast.length > 0 ? (
+          <div>
+            <List>
+              {cast.cast.map(({ cast_id, profile_path, name, character }) => (
+                <ListItem key={cast_id}>
+                  <Photo
+                    src={
+                      profile_path
+                        ? `${IMAGE_BASE_URL}${profile_path}`
+                        : `${PHOTO_PLACEHOLDER}`
+                    }
+                    alt=""
+                  />
+                  <Text>{name}</Text>
+                  <Text>
+                    <b>Character</b>: {character}
+                  </Text>
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        ) : (
+          <p>
+            Sorry, we didn't find any information about the cast of this movie
+          </p>
+        )}
+      </>
     )
   );
 };
